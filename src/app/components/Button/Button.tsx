1a23eaf0f1d5ac119ecb5fc2c589e0c2aca5afd2
Shwethaa Rajesh <shwethaarajesh14@gmail.com>
2024-04-11 17:11:01 +0530

    Feat : Added skeleton of home page

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -7,7 +7,13 @@ export default function Button(props: {
 }) {
   return (
     <div className="">
-      <button className="bg-secondary-light px-5 py-2 font-sans rounded-lg font-light text-sm border shadow-md hover:shadow-xl">
+      <button
+        className={
+          props.className
+            ? props.className
+            : "bg-secondary-light px-5 py-2 font-sans rounded-lg font-light text-sm border shadow-md hover:shadow-xl"
+        }
+      >
         {props.text}
       </button>
     </div>
--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -34,10 +34,15 @@ export default function HomePage() {
       <div className="m-6">
         <div className="flex flex-col gap-4">
           <FeaturedListings></FeaturedListings>
-          <div className="flex px-2 gap-5 justify-evenly items-center bg-primary-light">
-            <div className="w-8/12">color block with picture</div>
-            <div className="w-4/12 p-12 bg-secondary-light">
-              Button to create a posting
+
+          <div className="grid grid-cols-12 px-2 gap-5 font-sans font-light justify-evenly items-center border shadow-secondary-light border-secondary-light">
+            <div className="col-span-8">3 lines about project i</div>
+            <div className=" col-span-4 p-3 flex gap-12 flex-col justify-center items-center bg-primary-light ">
+              <div className="mt-4">Have an event in mind? </div>
+              <Button
+                text="Get Started"
+                className="bg-tertiary-light text-white px-5 py-2 font-sans rounded-lg font-normal text-sm border hover:shadow-md"
+              ></Button>
             </div>
           </div>
 
