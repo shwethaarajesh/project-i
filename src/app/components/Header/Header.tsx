937be8e0dbbdc68f203971aad860530a0f6eae64
Shwethaa Rajesh <shwethaarajesh14@gmail.com>
2024-06-27 13:14:31 +0530

    Feat: Added header border

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -3,9 +3,12 @@ import { CgProfile } from "react-icons/cg";
 import { GoHome } from "react-icons/go";
 import { IoMdArrowDropdown } from "react-icons/io";
 
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 export default function Header() {
   const router = useRouter();
+
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <div className="grid  gap-4 font-sans grid-cols-12 font-light items-center ">
       <button
@@ -17,7 +20,11 @@ export default function Header() {
       </button>
       <div className=" flex gap-4 col-span-10 justify-center text-sm">
         <div
-          className="p-3 hover:cursor-pointer border-b-2 border-transparent hover:border-secondary-light"
+          className={`p-3 hover:cursor-pointer border-b-2 ${
+            pathname == "/CreateListing"
+              ? "border-secondary-light"
+              : "border-transparent"
+          }  hover:border-secondary-light`}
           onClick={() => {
             router.push("/CreateListing");
           }}
@@ -25,7 +32,11 @@ export default function Header() {
           Create Event
         </div>
         <div
-          className="p-3 hover:cursor-pointer border-b-2 border-transparent hover:border-secondary-light"
+          className={`p-3 hover:cursor-pointer border-b-2 ${
+            pathname == "/ViewListing"
+              ? "border-secondary-light"
+              : "border-transparent"
+          }  hover:border-secondary-light`}
           onClick={() => {
             router.push("/ViewListing");
           }}
